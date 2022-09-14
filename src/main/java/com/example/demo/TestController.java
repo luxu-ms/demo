085b082ab286f1930e4a68069d3fe419123f9512
Lyle Xu <chanxl009@gmail.com>
2022-09-14 13:36:06 +0800

    init

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("test")
+@RestController
+public class TestController {
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello VS Code !!!";
+    }
+}
